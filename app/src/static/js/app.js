e67a3c252983cb6cb715e24f5ea554f1babc07f9
VerderioCristiano <149155006+VerderioCristiano@users.noreply.github.com>
2025-05-15 16:33:42 +0000

    cambio testo

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">you havo no to do items yet, add one!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
